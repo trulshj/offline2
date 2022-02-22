bbc48f4334795126143d41d7a4d35139acb631ac
Truls Henrik Jakobsen <trulshj@stud.ntnu.no>
2022-02-22 15:10:46 +0100

    added contribs

--- lib/authors.tsx
+++ lib/authors.tsx
@@ -7,182 +7,151 @@ export interface AuthorType {
   github?: string;
 }
 
-const lillachan: AuthorType = {
+export const alex: AuthorType = {
+  name: "Alex",
+  ao3: "https://archiveofourown.org/users/Vanya_Instance",
+};
+
+export const au_ruler: AuthorType = {
+  name: "au_ruler",
+  tumblr: "https://archester-creations.tumblr.com/",
+  ao3: "https://archiveofourown.org/users/AU_Ruler",
+};
+
+export const Cavalierious: AuthorType = {
+  name: "Cavalierious",
+  twitter: "https://twitter.com/_Cavalierious_",
+  ao3: "https://archiveofourown.org/users/Cavalierious/pseuds/Cavalierious"
+};
+
+export const coldsobashouto: AuthorType = {
+  name: "coldsobashouto",
+  ao3: "https://archiveofourown.org/users/coldsobashouto/pseuds/coldsobashouto",
+  twitter: "https://twitter.com/coldsobashouto",
+};
+
+export const kayloyal: AuthorType = {
+  name: "kayloyal",
+  twitter: "https://twitter.com/pancakebatter28",
+  ao3: "https://archiveofourown.org/users/Shadow_of_a_Raven/pseuds/Shadow_of_a_Raven",
+};
+
+export const Dev: AuthorType = {
+  name: "Dev",
+  twitter: "https://twitter.com/lxporidae",
+  tumblr: "https://lxporidae.tumblr.com/"
+};
+
+export const StardustCrow: AuthorType = {
+  name: "StardustCrow",
+  twitter: "https://twitter.com/nocturnesamurai",
+  instagram: "https://www.instagram.com/stardustcrow/"
+};
+
+export const Sunflowersailor: AuthorType = {
+  name: "Sunflowersailor",
+  twitter: "https://twitter.com/Sunfl0wersailor",
+};
+
+export const tyytyy: AuthorType = {
+  name: "tyytyy",
+  ao3: "https://archiveofourown.org/users/TyyTyy/pseuds/TyyTyy",
+  twitter: "https://twitter.com/ss_tyytyy",
+};
+
+export const Vic: AuthorType = {
+  name: "Vic",
+  twitter: "https://twitter.com/babybluebells",
+  ao3: "https://archiveofourown.org/users/babybluebells/pseuds/babybluebells"
+};
+
+export const Mingyss: AuthorType = {
+  name: "Mingyss",
+  twitter: "https://twitter.com/minmingyss",
+  tumblr: "https://mingyss.tumblr.com/",
+  instagram: "https://www.instagram.com/frecklednerdmingyss/"
+};
+export const Auritry: AuthorType = {
+  name: "Auritry",
+  twitter: "https://twitter.com/girgitii",
+};
+export const Bignewts: AuthorType = {
+  name: "amuk",
+  twitter: "https://twitter.com/bignewts",
+  instagram: "https://www.instagram.com/bignewts/",
+};
+const bubunji: AuthorType = {
+  name: "bubun.ji",
+  twitter: "https://twitter.com/bubun_ji",
+  instagram: "https://www.instagram.com/bubun.ji/",
+};
+export const Cheshire: AuthorType = {
+  name: "Cheshire",
+  instagram: "https://www.instagram.com/r59k6/",
+  twitter: "https://twitter.com/r59k6",
+  tumblr: "https://r59k6.tumblr.com/"
+};
+export const Congercine: AuthorType = {
+  name: "Congercine",
+  twitter: "https://twitter.com/congercine",
+  instagram: "https://www.instagram.com/congercine/",
+};
+export const Fruichus: AuthorType = {
+  name: "Fruichus",
+  tumblr: "https://fruichus.tumblr.com/",
+  instagram: "https://www.instagram.com/fruichus/",
+  twitter: "https://twitter.com/fruichus"
+
+};
+export const Hyourin7372: AuthorType = {
+  name: "Hyourin7372",
+  twitter: "https://twitter.com/Hyourin7372",
+};
+export const krayonela : AuthorType = {
+  name: "krayonela ",
+  twitter: "https://twitter.com/krynl_",
+  instagram: "https://www.instagram.com/krayonela/",
+};
+export const Lenny: AuthorType = {
+  name: "Lenny",
+  instagram: "https://www.instagram.com/meikiran/",
+};
+export const lex_catto: AuthorType = {
+  name: "lex_catto",
+  twitter: "https://twitter.com/lex_catto",
+};
+export const lillachan: AuthorType = {
   name: "lillachan",
   twitter: "https://twitter.com/Lilla_Chan",
 };
-
-const bad: AuthorType = {
-  name: "BAD BAD TIME",
-  twitter: "http://twitter.com/badbadtime",
+export const Theo: AuthorType = {
+  name: "Theo",
+  twitter: "https://twitter.com/mira_t0ri",
+  instagram: "https://www.instagram.com/mira.t0ri/",
 };
-
-const ludoscope: AuthorType = {
-  name: "Lu",
+export const Momo: AuthorType = {
+  name: "Momo",
+  twitter: "https://twitter.com/dakumomo18",
+  tumblr: "https://dakumomo.tumblr.com/",
 };
-
-const kamedori: AuthorType = {
-  name: "Kame-dori",
-  tumblr: "https://kame-dori.tumblr.com/",
-  instagram: "https://www.instagram.com/kame.dori/",
-  twitter: "https://twitter.com/k456d0",
+export const neaekis: AuthorType = {
+  name: "neaekis",
+  twitter: "https://twitter.com/itsneaekis",
+  instagram: "https://www.instagram.com/neaekis/",
 };
-
-const tyulezhik: AuthorType = {
-  name: "Tyulezhik",
-  twitter: "https://twitter.com/tyulezhiktyulezhik",
-  instagram: "https://www.instagram.com/tyulezhik/",
+export const pixelflowerss: AuthorType = {
+  name: "pixelflowerss",
+  twitter: "https://twitter.com/GACKPOlD",
+  instagram: "https://www.instagram.com/pixelflowerss/",
 };
-
-const grandTaco: AuthorType = {
-  name: "GrandTaco",
-  twitter: "https://twitter.com/grandtacosuprem",
+export const Ridasu: AuthorType = {
+  name: "Ridasu",
+tumblr: "https://floofities.tumblr.com/",
+twitter: "https://twitter.com/floofities",
 };
-
-const miragePrince: AuthorType = {
-  name: "Prince",
-  twitter: "https://twitter.com/Mirage_Prince",
-};
-
-const burns: AuthorType = {
-  name: "KiingFluffybuns",
-  twitter: "https://twitter.com/kiingfluffybuns",
-};
-
-const crumbs: AuthorType = {
-  name: "artscrumbs",
-  instagram: "https://www.instagram.com/arts.crumbs/",
-  twitter: "https://twitter.com/artscrumbs",
-};
-
-const elianthos: AuthorType = {
-  name: "Elianthos",
-  ao3: "https://archiveofourown.org/users/elianthos/pseuds/elianthos",
-  twitter: "https://twitter.com/bowldeepfannish",
-  tumblr: "https://bowldeepfannish.tumblr.com/",
-};
-
-const krynnea: AuthorType = {
-  name: "Krynnea",
-  twitter: "https://twitter.com/krynnea",
-};
-
-const delacroiix: AuthorType = {
-  name: "Delacroiix",
-  twitter: "https://twitter.com/_delacroiix",
-  ao3: "https://archiveofourown.org/users/Delacroiix/pseuds/Delacroiix",
-};
-const smartZelda: AuthorType = {
-  name: "SmartZelda",
-  twitter: "https://twitter.com/SmartZelda",
-  tumblr: "https://smartzelda.tumblr.com/",
-};
-const rekil2s: AuthorType = {
-  name: "Reki-L2S",
-  twitter: "https://twitter.com/Morioh_ChoRadio",
-  tumblr: "https://crimsonherbarium.tumblr.com/",
-};
-const fishnobi: AuthorType = {
-  name: "fishnobi",
-  twitter: "https://twitter.com/fishnobi",
-  instagram: "https://www.instagram.com/fishnobi/",
-  tumblr: "https://fishnobi.tumblr.com/",
-  ao3: "https://archiveofourown.org/users/tuupo84/works",
-};
-const amuk: AuthorType = {
-  name: "amuk",
-  twitter: "https://twitter.com/amukWrites",
-  ao3: "https://archiveofourown.org/users/amuk",
-};
-const queenie: AuthorType = {
-  name: "Queenie",
-  twitter: "https://twitter.com/doctor_queenie",
-  ao3: "https://archiveofourown.org/users/QueenPotatos/pseuds/QueenPotatos",
-};
-const riyeRose: AuthorType = {
-  name: "Riye Rose",
-  twitter: "https://twitter.com/RiyeERose",
-};
-const discokonomi: AuthorType = {
-  name: "discokonomi",
-  twitter: "https://twitter.com/discokonomi",
-  ao3: "https://archiveofourown.org/users/discokonomi/pseuds/discokonomi",
-};
-const keitora: AuthorType = {
-  name: "keitora",
-};
-const singeiji: AuthorType = {
-  name: "singeiji",
-  twitter: "https://twitter.com/singeiji",
-  ao3: "https://archiveofourown.org/users/knoxoursavior/pseuds/knoxoursavior",
-};
-const lizard: AuthorType = {
-  name: "Lizard",
-  twitter: "https://twitter.com/ThatLizard1",
-  ao3: "https://archiveofourown.org/users/ThatLizard/pseuds/ThatLizard",
-};
-const twileighplants: AuthorType = {
-  name: "twileighplants",
-  twitter: "http://twitter.com/twileighplants",
-  tumblr: "https://twileighplants.tumblr.com/",
-  ao3: "https://archiveofourown.org/users/twileighplants/pseuds/twileighplants",
-};
-const queenclaw: AuthorType = {
-  name: "queenclaw",
-  twitter: "https://twitter.com/queenclaw_",
-  tumblr: "https://queenclaw-z.tumblr.com/",
-};
-const vanyaInstance: AuthorType = {
-  name: "Vanya_Instance",
-  ao3: "https://archiveofourown.org/users/Vanya_Instance",
-};
-const thechaoscryptid: AuthorType = {
-  name: "thechaoscryptid",
-  twitter: "https://twitter.com/thechaoscryptid",
-  ao3: "https://archiveofourown.org/users/thechaoscryptid/pseuds/thechaoscryptid",
-};
-const ness: AuthorType = {
-  name: "Ness",
-  ao3: "https://archiveofourown.org/users/tellmewhatyousee",
-  twitter: "https://twitter.com/matadorofheart",
-};
-const toxicTraitor: AuthorType = {
-  name: "ToxicTraitor",
-  twitter: "https://twitter.com/bytesoftoxicity",
-  ao3: "https://archiveofourown.org/users/ToxicTraitor/profile",
-};
-const unrivalling: AuthorType = {
-  name: "Unrivalling",
-};
-
-export {
-  lillachan,
-  bad,
-  ludoscope,
-  kamedori,
-  tyulezhik,
-  grandTaco,
-  miragePrince,
-  burns,
-  crumbs,
-  elianthos,
-  krynnea,
-  delacroiix,
-  smartZelda,
-  rekil2s,
-  fishnobi,
-  amuk,
-  queenie,
-  riyeRose,
-  discokonomi,
-  keitora,
-  singeiji,
-  lizard,
-  twileighplants,
-  queenclaw,
-  vanyaInstance,
-  thechaoscryptid,
-  ness,
-  toxicTraitor,
-  unrivalling,
+  export const SketchyPixel: AuthorType = {
+    name: "Sketchy Pixel",
+    instagram: "https://www.instagram.com/sketchy.pixel/",
+    tumblr: "https://sketchypixel.tumblr.com/",
+    twitter: "https://twitter.com/Sketchy__Pixel"
 };

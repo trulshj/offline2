0bc6b9b6bfa287b4fd119d9fc63b37a2cb2b5b79
Truls Henrik Jakobsen <trulshj@stud.ntnu.no>
2022-02-22 18:05:34 +0100

    Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,22 @@ import styles from "../styles/Header.module.css";
 import Image from "next/image";
 
 import titleLogo from "../public/offline_logo.png";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const { asPath, pathname } = useRouter();
+
+  console.log(asPath);
+
   return (
     <header className={styles.header}>
-      <div className={styles.logoContainer}>
+      <div
+        className={
+          styles.logoContainer +
+          " " +
+          (asPath.includes("zine") ? styles.smallerHeader : null)
+        }
+      >
         <Image src={titleLogo} layout="responsive" alt="Adam in Love"></Image>
       </div>
     </header>
